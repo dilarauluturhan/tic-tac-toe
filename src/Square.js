0a4b7ed7fe9d9113a664f7b60dde143b8013f834
Dilara Uluturhan <dilarauluturhan@outlook.com>
2023-04-06 01:34:15 +0300

    Props added

---
+++ src/Square.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Square({ value }) {
+    return (
+        <>
+            <button className='square'>{value}</button>
+        </>
+    )
+}
+
+export default Square;
