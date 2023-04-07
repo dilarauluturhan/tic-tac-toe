722e91630ec6933c248642a334224e1f316bd3b0
Dilara Uluturhan <dilarauluturhan@outlook.com>
2023-04-07 15:59:10 +0300

    X-O-X transition added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { useState } from 'react'
 import Square from './Square';
 
-
-
 function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    if (squares[i]) {
+      return;
+    }
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
